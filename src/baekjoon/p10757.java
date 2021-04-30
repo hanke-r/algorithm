6e6785847a8dd443ac4492255542221ccaa69cb9
Han-Jaeok <hanjaeok17@illuvation.kr>
2021-04-30 09:16:51 +0900

    2021.04.30

---
+++ src/baekjoon/p10757.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class p10757 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+
+        System.out.println(A.add(B));
+    }
+}
